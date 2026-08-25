368f33ec744e886935ccfc3d0477c690f0265da2
AbdelmoumenBouslah230 <bobomoumen616@gmail.com>
2026-08-26 01:43:46 +0200

    Add files via upload

---
+++ tast5warizzmya.py
@@ -0,0 +1,26 @@
+min=[8, 20,0, 12, 16, 16,0,55]
+reels=[]
+ziro=min.count(0)
+for x in min:
+    if x!=0:
+        reels.append(x)
+        
+for i in range(ziro):
+    reels.append(0)
+
+print(reels)    
+    
+     
+    
+        
+        
+        
+       
+
+
+
+
+
+
+
+
